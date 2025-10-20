507dabb86fbfe26c9a3c2ccf3f16c894a801f2b8
ZeeNontakorn <73017121+ZeeNontakorn@users.noreply.github.com>
2025-10-21 00:58:09 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const db = require('./utils/db')
 
 const app = express()
 const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 app.use(cors())
 app.use(express.json())
