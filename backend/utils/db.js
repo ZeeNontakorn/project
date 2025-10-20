7166e3220849853f1d2bcb5b85d36506c21106b4
ZeeNontakorn <73017121+ZeeNontakorn@users.noreply.github.com>
2025-10-20 21:40:36 +0700

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'postgres.railway.internal',
+  host: 'turntable.proxy.rlwy.net',  // ✅ เปลี่ยนมาใช้ External Host
   database: 'railway',
   password: 'NjmovpvRWchLQnuzKnVbygZTIZsRXVYG',
-  port: 5432,
+  port: 50573,  // ✅ ใช้ External Port
 });
 
 module.exports = pool;
